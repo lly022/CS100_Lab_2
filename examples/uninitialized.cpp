07d60dcd6c2efefcc826e8194302c34406e8b017
lly022 <lly022@ucr.edu>
2020-10-12 23:13:16 -0700

    Update uninitialized.cpp, .gitignore

--- examples/uninitialized.cpp
+++ examples/uninitialized.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main() {
-    int x;
+    int x = 10;
     cout << x << endl;
 
     return 0;
-}
+} 
+
+//The following program was experimented with according to the lab manual
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    bool z;
+    int y = x + 5;
+    if (x) {
+        cout << "X is non-zero" << endl;
+    }
+    if (z) {
+        cout << "Z is truthy" << endl;
+    }
+    cout << y << endl;
+    return 0;
+}*/
+ 
